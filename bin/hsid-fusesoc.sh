4546b59c4fb2e579ccfa37e6874cd0e2666ebf07
Pablo Palazon <pablo.palazon@gmail.com>
2025-07-04 20:40:13 +0200

    Add fusesoc cores for all submodules

---
+++ bin/hsid-fusesoc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e # Exit on error
+set -x # Enable verbose command output
+
+SHOW_WAVEFORM=false
+while getopts "w" opt; do
+  case $opt in
+    w) SHOW_WAVEFORM=true;;
+    \?) echo "Invalid option: -$OPTARG" >&2 ;;
+  esac
+done
+
+# Remove options from positional parameters
+shift $((OPTIND - 1))
+
+MODULE_NAME="$1"
+
+BASE_DIR=$(pwd)
+WAVES=${BASE_DIR}/hw/waves
+
+FUSESOC_NAME="uclm:hspecidx:${MODULE_NAME}:1.0.0"
+FUSESOC_SIM_DIR="build/uclm_hspecidx_${MODULE_NAME}_1.0.0/sim-xsim"
+
+fusesoc run --no-export --target sim $FUSESOC_NAME
+
+if [ "$SHOW_WAVEFORM" = true ]; then
+  sed -E 's/\$scope module ([^ ]+)\(.*\) /\$scope module \1 /' $FUSESOC_SIM_DIR/wave.vcd > $FUSESOC_SIM_DIR/cleaned.vcd
+  gtkwave $FUSESOC_SIM_DIR/cleaned.vcd $WAVES/$MODULE_NAME.gtkw > /dev/null 2>&1 &
+else
+  echo "Simulation completed. Use -w option to view waveform."
+fi
