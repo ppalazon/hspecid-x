56e1b56d80dd42bcd7576723851b08a224bbddb4
Pablo Palazon <pablo.palazon@gmail.com>
2025-08-11 20:34:12 +0200

    Improve sim scripts and gtkwave files

--- bin/hsid-vsim.sh
+++ bin/hsid-vsim.sh
@@ -2,13 +2,35 @@
 set -e # Exit on error
 set -x # Enable verbose command output
 
+SHOW_GUI=false
+while getopts "g" opt; do
+  case $opt in
+    g) SHOW_GUI=true;;
+    \?) echo "Invalid option: -$OPTARG" >&2 ;;
+  esac
+done
+
+# Remove options from positional parameters
+shift $((OPTIND - 1))
+
 MODULE_NAME="$1"
 
 BASE_DIR=$(pwd)
-WAVES=${BASE_DIR}/hw/waves
+MODELSIM_DO=${BASE_DIR}/hw/tcl/modelsim-do
 
 FUSESOC_NAME="uclm:hspecidx:${MODULE_NAME}"
-FUSESOC_SIM_DIR="build/uclm_hspecidx_${MODULE_NAME}/sim-modelsim"
+FUSESOC_SIM_DIR="build/uclm_hspecidx_${MODULE_NAME}_0/sim-modelsim"
 
-fusesoc run --no-export --target sim --tool modelsim $FUSESOC_NAME
 
+if [ "$SHOW_GUI" = true ]; then
+  if [ ! -d "$FUSESOC_SIM_DIR" ]; then
+    echo "Simulation directory $FUSESOC_SIM_DIR does not exist. Please run 'hsid-vsim $MODULE_NAME'."
+    exit 1
+  fi
+
+  # Open modelsim gui
+  cd $FUSESOC_SIM_DIR
+  make run-gui
+else
+  fusesoc run --no-export --target sim --tool modelsim $FUSESOC_NAME
+fi
--- bin/hsid-xsim.sh
+++ bin/hsid-xsim.sh
@@ -2,10 +2,12 @@
 set -e # Exit on error
 set -x # Enable verbose command output
 
+SHOW_GUI=false
 SHOW_WAVEFORM=false
-while getopts "w" opt; do
+while getopts "wg" opt; do
   case $opt in
     w) SHOW_WAVEFORM=true;;
+    g) SHOW_GUI=true;;
     \?) echo "Invalid option: -$OPTARG" >&2 ;;
   esac
 done
@@ -21,11 +23,22 @@ WAVES=${BASE_DIR}/hw/waves
 FUSESOC_NAME="uclm:hspecidx:${MODULE_NAME}"
 FUSESOC_SIM_DIR="build/uclm_hspecidx_${MODULE_NAME}_0/sim-xsim"
 
-fusesoc run --no-export --target sim --tool xsim $FUSESOC_NAME
+if [ "$SHOW_GUI" = true ]; then
+  if [ ! -d "$FUSESOC_SIM_DIR" ]; then
+    echo "Simulation directory $FUSESOC_SIM_DIR does not exist. Please run 'hsid-xsim $MODULE_NAME'."
+    exit 1
+  fi
 
-if [ "$SHOW_WAVEFORM" = true ]; then
-  sed -E 's/\$scope module ([^ ]+)\(.*\) /\$scope module \1 /' $FUSESOC_SIM_DIR/wave.vcd > $FUSESOC_SIM_DIR/cleaned.vcd
-  gtkwave $FUSESOC_SIM_DIR/cleaned.vcd $WAVES/$MODULE_NAME.gtkw > /dev/null 2>&1 &
+  # Open xsim gui
+  cd $FUSESOC_SIM_DIR
+  make run-gui
 else
-  echo "Simulation completed. Use -w option to view waveform."
+  fusesoc run --no-export --target sim --tool xsim $FUSESOC_NAME
+  if [ "$SHOW_WAVEFORM" = true ]; then
+    sed -E 's/\$scope module ([^ ]+)\(.*\) /\$scope module \1 /' $FUSESOC_SIM_DIR/wave.vcd > $FUSESOC_SIM_DIR/cleaned.vcd
+    gtkwave $FUSESOC_SIM_DIR/cleaned.vcd $WAVES/$MODULE_NAME.gtkw > /dev/null 2>&1 &
+  else
+    echo "Simulation completed. Use -w option to view waveform."
+  fi
+  
 fi
